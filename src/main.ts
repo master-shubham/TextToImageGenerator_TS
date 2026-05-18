296af836d48fc0c9eefe56569dbdb814898561ff
Master Shubham <ommylight@gmail.com>
2026-05-18 18:01:38 +0530

    feat: adding dark/light Theme Toggle

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
-import './style.css'
+import "./style.css";
+
+const themeToggle = document.querySelector<HTMLButtonElement>(".theme-toggle");
+
+// Set theme based on saved preference or system default
+(()=>{
+   const savedTheme= localStorage.getItem("theme");
+   const systemPrefersDark:boolean =window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+   const isDarkTheme:boolean = savedTheme === "dark" || (!savedTheme && systemPrefersDark);
+      document.body.classList.toggle("dark-theme",isDarkTheme);
+      const icon = themeToggle?.querySelector("i");
+      if (icon) {
+        icon.className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
+      }
+
+})()
+
+
+// Switch between light and dark themes
+const toggleTheme = () => {
+    const isDarkTheme: boolean = document.body.classList.toggle("dark-theme");
+    localStorage.setItem("theme",isDarkTheme ? "dark":"light")
+    const icon =themeToggle?.querySelector("i")
+    if (icon) {
+        icon.className = isDarkTheme
+          ? "fa-solid fa-sun"
+          : "fa-solid fa-moon";
+    }
+};
+
+themeToggle?.addEventListener("click", toggleTheme);
